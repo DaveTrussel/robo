7273a492289734c8571f8cf3209606d3fb0f859f
= <d.crucel@gmail.com>
2017-05-01 12:29:27 +0200

    added initalizer befor << token

--- src/test.cpp
+++ src/test.cpp
@@ -9,10 +9,12 @@
 using namespace robo
 
 int main () {
-	Eigen::Vector3d axis << 0.0, 0.0, 1.0;
+	Eigen::Vector3d axis;
+	axis << 0.0, 0.0, 1.0;
 	Frame f = Frame();
 	Joint joint_1 = Joint(1, f, axis, JointType::Rotational); 
-	Eigen::Vector3d length << 1.0, 1.0, 1.0;
+	Eigen::Vector3d length;
+	length << 1.0, 1.0, 1.0;
 	Frame tip = Frame(length);
 	Link link_01 = Link(1, joint_1, tip);
  } 
