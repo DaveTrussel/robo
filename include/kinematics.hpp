3e97da87ae22d1fc6d2d270eff761b2e57e6cff7
= <d.crucel@gmail.com>
2017-05-06 15:14:58 +0200

    IK algorithm implementation start

--- include/kinematics.hpp
+++ include/kinematics.hpp
@@ -10,17 +10,46 @@ namespace robo{
 	public:
 		Chain chain;
 		Frame f_end;
+		VectorXd q;
+		std::vector<Frame> link_tips;
 		std::vector<Frame> joint_roots;
 		std::vector<Frame> joint_tips;
 		Eigen::MatrixXd jacobian;
 		Eigen::JacobiSVD<MatrixXd> svd;
+		Vector6d L;
+		int max_iter;
+		double eps;
+    	double eps_joints;
 
-		Kinematics(const Chain& chain);
+    	static const int E_NO_ERROR = 					 1;
+    	static const int E_JOINTS_GRADIENT_TOO_SMALL =  -1;
+    	static const int E_JOINTS_INCREMENT_TOO_SMALL = -2;
+    	static const int E_MAX_ITERATIONS = 			-3;
 		
-		void joint_to_cartesian(const Eigen::VectorXd& q, std::vector<Frame>& f_out);
+
+		Kinematics(const Chain& chain,
+				   int max_iter=500,
+				   double eps=1e-5,
+				   double eps_joints=1e-10);
+
+		Kinematics(const Chain& chain,
+				   Vector6d L,
+				   int max_iter=500,
+				   double eps=1e-5,
+				   double eps_joints=1e-10);
+		
+		void joint_to_cartesian(const Eigen::VectorXd& q);
+
+		void cartesian_to_joint(const Frame& f_in, Eigen::VectorXd& q);
 
 		void calculate_jacobian(const Eigen::VectorXd& q);
 		
+	private:
+		VectorXd q_new;
+		VectorXq tmp;
+		VectorXq delta_q;
+		VectorXq grad;
+		VectorXq singular_vals; // TODO rename
 	};
 
 }	
--- src/kinematics.cpp
+++ src/kinematics.cpp
@@ -3,12 +3,19 @@
 
 namespace robo{
 
-	Kinematics::Kinematics(const Chain& chain_):
+	Kinematics::Kinematics(const Chain& chain_, int max_iter_, double eps_, double eps_joints_):
 	chain(chain_), f_end(), joint_roots(chain_.nr_joints), joint_tips(chain_.nr_joints),
-	jacobian(6, chain_.nr_joints), svd(6, nj,Eigen::ComputeThinU | Eigen::ComputeThinV)
-	{}
+	jacobian(6, chain_.nr_joints), svd(6, nj,Eigen::ComputeThinU | Eigen::ComputeThinV),
+	max_iter(max_iter), eps(eps_), eps_joints(eps_joints_)
+	{
+		L << 1, 1, 1, 0.1, 0.1, 0.1;
+	}
+	Kinematics::Kinematics(const Chain& chain_, Vector6d L_, int max_iter_, double eps_, double eps_joints_):
+	chain(chain_), f_end(), joint_roots(chain_.nr_joints), joint_tips(chain_.nr_joints),
+	jacobian(6, chain_.nr_joints), svd(6, nj,Eigen::ComputeThinU | Eigen::ComputeThinV),
+	max_iter(max_iter), eps(eps_), eps_joints(eps_joints_), L(L_){}
 	
-	void Kinematics::joint_to_cartesian(const Eigen::VectorXd& q, std::vector<Frame>& f_out){
+	void Kinematics::joint_to_cartesian(const Eigen::VectorXd& q){
 		// init
 		int iter_joint = 0;
 		f_end = Frame();
@@ -22,7 +29,83 @@ namespace robo{
 			else{
 				f_end = f_end*chain.links[iter_link].pose(0.0);
 			}
-			f_out[iter_link] = f_end;
+			link_tips[iter_link] = f_end;
+		}
+	}
+
+	int Kinematics::cartesian_to_joint(const Frame& f_in, const Eigen::VectorXd& q_init){
+		double v      = 2;
+		double tau    = 10;
+		double rho;
+		double norm_delta_twist;
+		double norm_delta_twist_new;
+		Vector6d twist;
+		Vector6d delta_twist;
+		Vector6d delta_twist_new;
+
+		q = q_init;
+		joint_to_cartesian(q);
+		delta_twist = f_end - f_in;
+		delta_twist = L.asDiagonal() * delta_twist;
+		norm_delta_twist = delta_twist.norm()
+		if(norm_delta_twist < esp){
+			delta_twist = f_end - f_in;
+			svd.compute(jacobian);
+			q_out = q;
+			return (error = E_NO_ERROR);
+		}
+		calculate_jacobian(q);
+		jacobian = L.asDiagonal() * jacobian;
+
+		double lambda = tau;
+		double dnorm = 1;
+
+		for(int i=0; i<max_iter, i++){
+			svd.compute(jacobian);
+			singular_vals = svd.singularValues();
+			for(int j=0; j<singular_vals.rows(); ++j){
+				singular_vals(j) = singular_vals(j) / (singular_vals(j) * singular_vals(j) + lambda);
+			}
+			tmp = svd.matrixU().transpose() * delta_twist;
+			tmp = singular_vals.cwiseProduct(tmp);
+			delta_q = svd.matrixV() * tmp;
+			grad = jacobian.transpose() * delta_twist;
+			dnorm = diffq.lpNorm<Eigen::Infinity>();
+			// check for errors
+			if(dnorm < eps_joints){
+				return (error = E_JOINT_INCREMENT_TOO_SMALL);
+			}
+			if(grad.transpose() * grad < eps_joints * eps_joints){
+				return (error = E_JONT_GRADIENT_TOO_SMALL);
+			}
+
+			q_new = q + delta_q;
+			joint_to_cartesian(q);
+			delta_twist_new = f_end - f_in;
+			norm_delta_twist_new = delta_twist_new.norm();
+			rho = norm_delta_twist * norm_delta_twist - norm_delta_twist_new * norm_delta_twist_new;
+			rho /= delta_q.transpose() * (lambda * delta_q + grad);
+			if (rho > 0) {
+				q               = q_new;
+				delta_twist       = delta_twist_new;
+				delta_twist_norm  = delta_twist_new_norm;
+				if (delta_twist_norm < eps) {
+					delta_twist = f_end - f_in;
+					q_out = q;
+					return (error = E_NO_ERROR);
+				}
+				calculate_jacobian(q_new);
+				jacobian = L.asDiagonal() * jacobian;
+				double tmp = 2 * rho - 1;
+				lambda = lambda * max(1 / 3.0, 1-tmp*tmp*tmp);
+				v = 2;
+			} 
+			else {
+				lambda = lambda * v;
+				v      = 2 * v;
+			}
+			q_out = q;
+			return (error = E_MAX_ITERATIONS);
 		}
 	}
 
