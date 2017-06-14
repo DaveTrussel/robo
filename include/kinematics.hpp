32216b0ca5d1995cb8c157b152539b070c475736
Dave <d.crucel@gmail.com>
2017-06-14 11:32:45 +0200

    MOD: minor refactoring of kinematics class

--- include/kinematics.hpp
+++ include/kinematics.hpp
@@ -10,21 +10,18 @@ namespace robo{
 	public:
 		// Members
 		Chain chain;
-		Frame f_end;
-		Eigen::VectorXd q;
-		Eigen::VectorXd q_out;
-		std::vector<Frame> link_tips;
-		std::vector<Frame> joint_roots;
-		std::vector<Frame> joint_tips;
+		Frame f_end; // frame at the end of the chain 
+		Eigen::VectorXd q_out; // result of inverse kinematics is stored here
+		std::vector<Frame> link_tips; // Frames at end of each link
 		Eigen::MatrixXd jacobian;
-		Eigen::JacobiSVD<Eigen::MatrixXd> svd;
-		Vector6d L;
+		Vector6d weights_IK; // weigths of the IK algorithm (3 position and 3 orientation)
 		int max_iter;
 		double eps;
     	double eps_joints;
     	int error;
 
-    	// Error code constants
+    	// Error code constants 
+    	// TODO make this an enum class for clarity e.g. InvKinStatus::no_error?
     	static constexpr int E_NO_ERROR = 					 1;
     	static constexpr int E_JOINTS_GRADIENT_TOO_SMALL =  -1;
     	static constexpr int E_JOINTS_INCREMENT_TOO_SMALL = -2;
@@ -32,29 +29,46 @@ namespace robo{
 		
     	// Constructors
 		Kinematics(const Chain& chain,
-                   int max_iter=750,
+                   int max_iter=500,
                    double eps=1e-6,
-                   double eps_joints=1e-15);
+                   double eps_joints=1e-16);
 
-		Kinematics(const Chain& chain, Vector6d L,
-                   int max_iter=750,
+		Kinematics(const Chain& chain, Vector6d weights_IK,
+                   int max_iter=500,
                    double eps=1e-6,
-                   double eps_joints=1e-15);
+                   double eps_joints=1e-16);
 		
 		// Member functions
 		void joint_to_cartesian(const Eigen::VectorXd& q);
+		/**
+		* Calculates the forward kinematics
+		* The result is stored in link_tips and f_end
+		*/
 
 		int cartesian_to_joint(const Frame& f_in, const Eigen::VectorXd& q_init);
+		/**
+		* Calculates the inverse kinematics
+		* The result is stored in q_out
+		* Check the return value to see if the solver was sucessfull 
+		*/
 
 		void calculate_jacobian(const Eigen::VectorXd& q);
+		/**
+		* Calculates the jacobian
+		* The result is stored in jacobian
+		*/
 		
 	private:
 		// Members
+		Eigen::VectorXd q; // Joint positions
 		Eigen::VectorXd q_new;
-		Eigen::VectorXd tmp;
+		Eigen::VectorXd tmp_q;
 		Eigen::VectorXd delta_q;
-		Eigen::VectorXd grad;
+		Eigen::VectorXd grad; // gradient
 		Eigen::VectorXd singular_vals; // TODO rename
+		std::vector<Frame> joint_roots;
+		std::vector<Frame> joint_tips;
+		Eigen::JacobiSVD<Eigen::MatrixXd> svd;
 	};
 
 }	
--- src/kinematics.cpp
+++ src/kinematics.cpp
@@ -9,12 +9,13 @@ namespace robo{
 	jacobian(6, chain_.nr_joints), svd(6, chain_.nr_joints,Eigen::ComputeThinU | Eigen::ComputeThinV),
     max_iter(max_iter_), eps(eps_), eps_joints(eps_joints_)
 	{
-		L << 1, 1, 1, 0.1, 0.1, 0.1;
+		weights_IK << 1, 1, 1, 0.1, 0.1, 0.1;
 	}
-	Kinematics::Kinematics(const Chain& chain_, Vector6d L_, int max_iter_, double eps_, double eps_joints_):
+	
+	Kinematics::Kinematics(const Chain& chain_, Vector6d weigths_IK_, int max_iter_, double eps_, double eps_joints_):
     chain(chain_), f_end(), joint_roots(chain_.nr_joints), joint_tips(chain_.nr_joints), link_tips(chain.nr_links),
 	jacobian(6, chain_.nr_joints), svd(6, chain_.nr_joints,Eigen::ComputeThinU | Eigen::ComputeThinV),
-    max_iter(max_iter_), eps(eps_), eps_joints(eps_joints_), L(L_){}
+    max_iter(max_iter_), eps(eps_), eps_joints(eps_joints_), weights_IK(weigths_IK_){}
 	
 	// Member functions
 	void Kinematics::joint_to_cartesian(const Eigen::VectorXd& q){
@@ -23,12 +24,12 @@ namespace robo{
 		for(int iter_link=0; iter_link<chain.nr_links; iter_link++){
 			if(chain.links[iter_link].has_joint()){
 				joint_roots[iter_joint] = f_end;
-				f_end = f_end*chain.links[iter_link].pose(q[iter_joint]);
+				f_end = f_end * chain.links[iter_link].pose(q[iter_joint]);
 				joint_tips[iter_joint] = f_end;
 				iter_joint++;
 			}
 			else{
-				f_end = f_end*chain.links[iter_link].pose(0.0);
+				f_end = f_end * chain.links[iter_link].pose(0.0);
 			}
 			link_tips[iter_link] = f_end;
 		}
@@ -40,33 +41,35 @@ namespace robo{
 		q = q_init;
 		joint_to_cartesian(q);
 		delta_twist = f_end - f_in;
-		delta_twist = L.asDiagonal() * delta_twist;
+		delta_twist = weigths_IK.asDiagonal() * delta_twist;
 		double norm_delta_twist = delta_twist.norm();
+		calculate_jacobian(q);
+		
 		if(norm_delta_twist < eps){
 			delta_twist = f_end - f_in;
 			svd.compute(jacobian);
 			q_out = q;
 			return (error = E_NO_ERROR);
 		}
-		calculate_jacobian(q);
-		jacobian = L.asDiagonal() * jacobian;
+		
+		Eigen::MatrixXd jacobian_weighted = weigths_IK.asDiagonal() * jacobian;
 
-		double v = 2;
+		double step_multiplier = 2;
 		double lambda = 10;
 		double dnorm = 1;
 		double norm_delta_twist_new;
 		double rho;
 
-		for(int i=0; i<max_iter; i++){
-			svd.compute(jacobian);
+		for(int i=0; i<max_iter; ++i){
+			svd.compute(jacobian_weighted);
 			singular_vals = svd.singularValues();
 			for(int j=0; j<singular_vals.rows(); ++j){
 				singular_vals(j) = singular_vals(j) / (singular_vals(j) * singular_vals(j) + lambda);
 			}
-			tmp = svd.matrixU().transpose() * delta_twist;
-			tmp = singular_vals.cwiseProduct(tmp);
-			delta_q = svd.matrixV() * tmp;
-			grad = jacobian.transpose() * delta_twist;
+			tmp_q = svd.matrixU().transpose() * delta_twist;
+			tmp_q = singular_vals.cwiseProduct(tmp_q);
+			delta_q = svd.matrixV() * tmp_q;
+			grad = jacobian_weighted.transpose() * delta_twist;
 			dnorm = delta_q.lpNorm<Eigen::Infinity>();
 			// check for errors
 			if(dnorm < eps_joints){
@@ -92,14 +95,14 @@ namespace robo{
 					return (error = E_NO_ERROR);
 				}
 				calculate_jacobian(q_new);
-				jacobian = L.asDiagonal() * jacobian;
+				jacobian_weighted = weigths_IK.asDiagonal() * jacobian;
 				double tmp = 2 * rho - 1;
 				lambda = lambda * std::max(1 / 3.0, 1-tmp*tmp*tmp);
-				v = 2;
+				step_multiplier = 2;
 			} 
 			else {
-				lambda = lambda * v;
-				v      = 2 * v;
+				lambda = lambda * step_multiplier;
+				step_multiplier = 2 * step_multiplier;
 			}
 			q_out = q;
 		}
