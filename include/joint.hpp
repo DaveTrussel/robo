ea25cf63adb7324692d5370c707d1ca772345a7a
Dave <d.crucel@gmail.com>
2017-06-19 14:26:16 +0200

    MOD: made Joint a header-only class

--- include/joint.hpp
+++ include/joint.hpp
@@ -20,15 +20,36 @@ namespace robo {
 
 		// Constructors
 		Joint(const int id_in, const Frame frame_in, const Eigen::Vector3d axis_in, 
-			const JointType type_in=JointType::Rotational);
+			  const JointType type_in=JointType::Rotational):
+			id(id_in), type(type_in), axis(axis_in), frame(frame_in){};
 
 		// Member functions
-		Frame pose(const double& q)const;
+		Frame pose(const double& q)const{
+			if(type == JointType::Rotational){
+				Eigen::Matrix3d rotation = (Eigen::Matrix3d)Eigen::AngleAxisd(q, axis);
+				return Frame(frame.origin, rotation);
+			}
+			if(type == JointType::Translational){ 
+				return Frame(frame.origin + q*axis, frame.orientation);
+			}
+			else{
+				return Frame(frame);
+			}
+		};
 
-		Twist twist(const double &dq)const;
-
-		Eigen::Matrix3d rotation(const double& q)const;
-
+		Twist twist(const double &dq)const{
+			Eigen::Vector3d speed_lin;
+			Eigen::Vector3d speed_rot;
+			speed_lin << 0.0, 0.0, 0.0;
+			speed_rot << 0.0, 0.0, 0.0;
+			if(type == JointType::Rotational){
+				speed_rot << dq*axis;
+			}
+			if(type == JointType::Translational){
+				speed_lin << dq*axis;
+			}
+			return Twist(speed_lin, speed_rot);
+		};
 	};
 
 }
